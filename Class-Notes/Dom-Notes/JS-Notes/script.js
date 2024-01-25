c36791ffda5755bb3e23d1e01762d46da6f6d4b9
CurtisHemm <146120781+CurtisHemm@users.noreply.github.com>
2024-01-25 16:55:33 -0500

    Create script.js

---
+++ Class-Notes/Dom-Notes/JS-Notes/script.js
@@ -0,0 +1,111 @@
+// <--------------------------- Functions ----------------------------->
+
+// Function Declaration
+// function add(a, b) {
+//     return a + b
+// }
+// // Execute the function below
+// add(3,5) // --> return B
+
+
+// console.log(num);
+
+// const num = 10;
+
+
+// // Function Expression
+// const add = function (a, b) {
+//     console.log(a + b);
+//     return a + b;
+// };
+// add(4,6) // ---> return 10
+
+// // Arrow Functions
+// const add = (a, b) => {
+//     return a + b;
+// };
+// add(10,5); // ---> return 15
+
+
+
+// <------- SPREAD OPERATOR -------->
+
+// const numbers = [1, 2, 3, 4, 5];
+// const numbers2 = [6, 7, 8, 9];
+// const numbersCopy = [...numbers];
+
+// console.log(numbersCopy);
+
+
+// <---------- For/Of Loop -------------->
+
+// for (num of numArray) {
+//     num += 100;
+//     console.log(num);
+// }
+
+// const numArray = [1, 2, 3, 4, 5];
+
+// for (num of numArray) {
+//     console.log(num);
+// }
+
+// const course = "client-side-scripting";
+
+// for (letter of course) {
+//     console.log(letter);
+// }
+
+// <---- JS Object ---->
+// const car = {
+//     type: "SUV",
+//     make: "Honda",
+//     year: 2015, 
+// };
+
+// console.log(car);
+// console.log(car.type);
+// console.log(car.year);
+
+// <---------- Maps ---------->
+
+// const fruits = new Map([
+//     ["apple", 500],
+//     ["bananas", 300],
+//     ["oranges", 200],
+// ]);
+
+// console.dir(fruits);
+// console.dir(fruits.get("bananas"));
+
+
+// <---- Sets ---->
+
+// const letters = new Set();
+// letters.add("a");
+// letters.add("a");
+// letters.add("a");
+// letters.add("b");
+// letters.add(3);
+// letters.add([1, 2, 3]);
+// letters.add({ key: "value" });
+// console.log(letters);
+
+
+// <------ Classes ------>
+class Animal {
+    constructor(species, numOfLegs, canSwim, canFly) {
+        this.species = species;
+        this.numOfLegs = numOfLegs;
+        this.canSwim = canSwim;
+        this.canFly = canFly;
+    }
+    meow() {
+        console.log("meow");
+    }
+}
+
+const cat = new Animal("Persian Cat", 4, true, false);
+
+console.log(cat);
+cat.meow();
