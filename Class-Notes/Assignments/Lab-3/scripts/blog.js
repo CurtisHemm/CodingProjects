b318b35a6d92bbd58a29638f0e5294c478f771bf
CurtisHemm <146120781+CurtisHemm@users.noreply.github.com>
2024-03-21 23:26:32 -0400

    Completely changed the js file to be asynchronous and fixed the images

--- Class-Notes/Assignments/Lab-3/scripts/blog.js
+++ Class-Notes/Assignments/Lab-3/scripts/blog.js
@@ -1,62 +1,44 @@
 //Curtis Hemminghaus, 100874539, 2024-03-14
 
-document.getElementById("loadBlogs").addEventListener("click", function () {
+// If currently on the blog page
+if(window.location.pathname == "/blog.html"){
 
-    // var xhr = new XMLHttpRequest();
-    // xhr.open("GET", "https://jsonplaceholder.typicode.com/posts?limit=20&offset=0");
-
-    // xhr.onload = function () {
-    // console.log("API has responded!");
-    // //   console.log(this);
-    // var response = JSON.parse(this.responseText);
-    // console.log(response);
-    // // var postString = "";
-    // // response.results.forEach((posts) => {
-    // //     postString += `<li>${posts.id}/li>`;
-    // // });
-    // // document.getElementById("postsList").innerHTML = postString;
-    // };
-    // xhr.send();
-
-    let postsLists = fetch(
-        "https://jsonplaceholder.typicode.com/posts?limit=20&offset=10",
+    // Make it asyncronous to the fetches can happan together
+    (async () => {
+    
+    // Fetch the json of the posts
+    let postsLists = await fetch(
+        "https://jsonplaceholder.typicode.com/posts?limit=20&offset=0",
         {
             method: "GET",
-        }
-    ).then((response) => {
-        return response.json();
-    }).then((jsonResult) => {
-        let first20 = jsonResult.slice(0,20);
-        console.log(first20);
+        },
+    );
+    // Fetch the images from the api
+    let imageLists = await fetch(
+        "https://pixabay.com/api/?key=42944494-fbd60779ac631f173709c01ab&q=yellow+flowers&image_type=photo/hits?limit=20&offset=0",
+        {
+            method: "GET",
+        },
+    )
 
-        document.getElementById('twenty-cards').innerHTML = '';
+    // Get the responses from the fetches
+    var jsonPosts = await postsLists.json();
+    var jsonImages = await imageLists.json();
 
-        //Making the 20 cards
-        first20.forEach((posts) => {
-            let cardCreation = `
+    // Get the twenty-cards element from the html page
+    document.getElementById('twenty-cards').innerHTML = '';
+
+    // For loop that loops 20 times creating cards with the titles and body from the posts, and images from pixabay 
+    for (let i = 0; i < 20; i++) {
+        let cardCreation = `
             <div class="card" style="width: 18rem;">
                 <div class="card-body">
-                    <h5 class="card-title">${posts.title}</h5>
-                    <img
-                    <p class="card-text">${posts.body}</p>
+                    <h5 class="card-title">${jsonPosts[i].title}</h5>
+                    <img class="card-image-middle" id="blogImage" src="${jsonImages.hits[i].userImageURL}" alt="Card image cap">
+                    <p class="card-text">${jsonPosts[i].body}</p>
                 </div>
             </div>
         `;
-        document.getElementById('twenty-cards').innerHTML += cardCreation;
-        });
-    })
-
-    let imageLists = fetch(
-        "https://pixabay.com/api/?key=42944494-fbd60779ac631f173709c01ab&q=yellow+flowers&image_type=photo",
-        {
-            method: "GET",
-        }
-    ).then((response) => {
-        return response.json();
-    }).then((jsonResult) => {
-        let twentyImages = jsonResult.slice(0,20);
-
-        twentyImages.forEach
-    })
-    
-});
+        document.getElementById('twenty-cards').innerHTML += cardCreation;               // Insert the card into the html
+    }
+    })()};
