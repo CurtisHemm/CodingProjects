80f57c8083d2651f5cd6375d5bf93e41aef86bb4
CurtisHemm <146120781+CurtisHemm@users.noreply.github.com>
2024-04-18 22:04:49 -0400

    Added some more documentation to the Lab 4

--- Class-Notes/Assignments/Lab-4/app.js
+++ Class-Notes/Assignments/Lab-4/app.js
@@ -11,6 +11,7 @@ require("dotenv").config();
 // Connect to db
 DBConfig.connectToDatabase();
 
+// Set the view engine for ejs 
 app.set("view engine", "ejs");
 
 // Middleware to register the 'public' folder as a static assest
@@ -20,6 +21,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Set the home page
 app.use("/", animalRouter);
 
 app.listen(process.env.PORT, () => {
--- Class-Notes/Assignments/Lab-4/controllers/animalControllers.js
+++ Class-Notes/Assignments/Lab-4/controllers/animalControllers.js
@@ -15,8 +15,8 @@ function displayAnimalEntry(req, res) {
 async function saveAnimalToCollection(req, res) {
     // Grab the form data
     const formData = req.body;
-    //Create a new pokemon in our database
 
+    // Create new Animal
     try {
         await Animal.create({
             Zoo: formData.zoo,
@@ -29,9 +29,11 @@ async function saveAnimalToCollection(req, res) {
             IsTransportable: formData.isTransportable
         });
     } catch (err){
+        //Error catching
         console.log(`Error in creating animal ${formData.animalSciName}`);
     }
-    res.redirect("/savedCollection"); // redirect user back to slash route upon completion
+    // Redirect to collection page
+    res.redirect("/savedCollection"); 
 }
 
 // Function to display all animals 
@@ -40,43 +42,48 @@ async function displayAllAnimals(req, res) {
         const myCollection = await Animal.find({});
         res.render("animals/all-animals.ejs", {myCollection, pageTitle: 'All Animals'});
     } catch (err){
+        // error catching
         console.log("Error with getting saved collection");
         res.status(500).send("Error in getting saved collection");
     }
 }
 
+// Function that takes the id of an animal and displays the edit page with that animal's data
 function displayEditPage(req, res) {
     try {
         const animalId = req.params.idOfAnimal; // Get the animal ID from request parameters
 
         // Find the animal in the database by its ID
         Animal.findById(animalId).then(animal => {
-            // Render the edit page with the retrieved animal data
+            // Rendering the edit page with the animal data
             res.render("animals/edit-animal.ejs", { pageTitle: 'Edit Animal', animal: animal });
         })
         .catch(err => {
-            // Handle any errors (e.g., database error)
+            // error catching
             console.error('Error finding animal:', err);
             res.status(500).send('Internal Server Error');
         });
     } catch (err) {
-        // Handle any synchronous errors
+        // error catching
         console.error('Error:', err);
         res.status(500).send('Internal Server Error');
     }
 }
 
+// Deleting the animal based on the id it will have in the db
 async function deleteAnimalById(req, res) {
     try {
         const animalId = req.params.idOfAnimal;
         await Animal.deleteOne({_id: animalId});
         res.redirect("/savedCollection");
     } catch (err) {
+        // error catching
         console.err("Error with getting saved collection");
         res.status(500).send("Error in getting saved collection");
     }
 }
 
+// Edit the animal based on the id it has in the database and redirect to the collection
 async function editAnimalById(req, res){
     try {
         const animalId = req.params.idOfAnimal; // Get the animal ID from request parameters
@@ -95,10 +102,8 @@ async function editAnimalById(req, res){
                 Age: animalAge,
                 IsTransportable: animalIsTransportable
             },
-            { new: true } // To return the updated document
+            { new: true } 
         );
-
-        // Redirect to a page displaying the updated animal information
         res.redirect("/savedCollection");
     } catch (err) {
         console.error('Error updating animal:', err);
@@ -106,6 +111,7 @@ async function editAnimalById(req, res){
     }
 }
 
+// Export all the functions 
 module.exports = {
     displayHomePage,
     displayAnimalEntry,
